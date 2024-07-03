5512843897cce5f7bf7d4dc12b29e775518e0425
Raghuram07 <raghuramreddy2599@gmail.com>
2024-07-03 19:49:34 -0400

    first commit

---
+++ video-chat/client/src/components/Messages.jsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react';
+import io from 'socket.io-client';
+
+// Assuming you have a socket connection established elsewhere and imported here
+const socket = io.connect('ws://localhost:5000');
+
+const MessagesDisplayComponent = () => {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    // Listen for new messages
+    socket.on('receiveMessage', (message) => {
+      // eslint-disable-next-line
+      console.log('New message received:', message);
+      // Update the state with the new message
+      setMessages((prevMessages) => [...prevMessages, message]);
+    });
+
+    // Cleanup on component unmount
+    return () => {
+      socket.off('receiveMessage');
+    };
+  }, []);
+
+  return (
+    <div>
+      <h2>Received Messages</h2>
+      <ul>
+        {messages.map((msg, index) => (
+          <li key={index}>{msg}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default MessagesDisplayComponent;
---
+++ video-chat/index.js
@@ -0,0 +1,44 @@
+const app = require("express")();
+const server = require("http").createServer(app);
+const cors = require("cors");
+
+const io = require("socket.io")(server, {
+	cors: {
+		origin: "*",
+		methods: [ "GET", "POST" ]
+	}
+});
+
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+	res.send('Running');
+});
+
+io.on("connection", (socket) => {
+
+	socket.emit("me", socket.id);
+
+	socket.on("disconnect", () => {
+		socket.broadcast.emit("callEnded")
+	});
+
+	socket.on("callUser", ({ userToCall, signalData, from, name }) => {
+		io.to(userToCall).emit("callUser", { signal: signalData, from, name });
+	});
+
+	socket.on("answerCall", (data) => {
+		io.to(data.to).emit("callAccepted", data.signal)
+	});
+
+	  // Listen for new messages
+	  socket.on("newMessage", (message) => {
+        console.log("New message received:", message);
+        socket.broadcast.emit("receiveMessage", message);
+    });
+
+});
+
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
